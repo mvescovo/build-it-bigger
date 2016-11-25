41ce16eb52856528c865a7fb1be927d2bd1044c6
Michael <m.vescovo@gmail.com>
2016-11-26 10:06:15 +1100

    feat: Display joke using display_joke lib
    From the main app, retrieve a joke from the java joke library, then send
    the joke an an intent extra to the display_joke android library for display.

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,13 +1,14 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
 import com.example.Joker;
+import com.example.display_joke.DisplayJokeActivity;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -43,8 +44,8 @@ public class MainActivity extends ActionBarActivity {
 
     public void tellJoke(View view) {
         String joke = new Joker().getJoke();
-        Toast.makeText(this, joke, Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, DisplayJokeActivity.class);
+        intent.putExtra(DisplayJokeActivity.EXTRA_JOKE, joke);
+        startActivity(intent);
     }
-
-
 }
