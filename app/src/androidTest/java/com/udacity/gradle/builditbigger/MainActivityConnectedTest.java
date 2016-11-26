f533c28e0fc1d0803193f028382faef4496b6e92
Michael <m.vescovo@gmail.com>
2016-11-26 16:22:53 +1100

    test: Add connected tests for non empty joke
    Check that strings returned from the joke async task are not empty.

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/MainActivityConnectedTest.java
@@ -0,0 +1,34 @@
+package com.udacity.gradle.builditbigger;
+
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.not;
+
+/**
+ * @author Michael Vescovo.
+ */
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityConnectedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testGetJokeNotEmptyString() {
+        onView(withId(R.id.tell_joke_button)).perform(click());
+        onView(withId(R.id.joke)).check(matches(not(withText(""))));
+    }
+}
