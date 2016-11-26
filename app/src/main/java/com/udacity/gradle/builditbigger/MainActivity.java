aa9a7ab9d4062792e735a6f99fd25708c81d41c0
Michael <m.vescovo@gmail.com>
2016-11-26 15:07:17 +1100

    feat: Point app to new cloud backend
    Backend is now published in the cloud. Instead of using the local
    backend, use the cloud instance available everywhere from all devices.

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -13,8 +13,6 @@ import com.example.display_joke.DisplayJokeActivity;
 import com.example.michael.jokes.backend.myApi.MyApi;
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
-import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
-import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 
 import java.io.IOException;
 
@@ -63,18 +61,7 @@ public class MainActivity extends ActionBarActivity {
             if (myApiService == null) {  // Only do this once
                 MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
                         new AndroidJsonFactory(), null)
-                        // options for running against local devappserver
-                        // - 10.0.2.2 is localhost's IP address in Android emulator
-                        // - turn off compression when running against local devappserver
-                        .setRootUrl("http://10.0.2.2:8080/_ah/api/")
-                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-                            @Override
-                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
-                                abstractGoogleClientRequest.setDisableGZipContent(true);
-                            }
-                        });
-                // end options for devappserver
-
+                        .setRootUrl("https://jokes-150703.appspot.com/_ah/api/");
                 myApiService = builder.build();
             }
 
