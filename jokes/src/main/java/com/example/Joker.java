8d09556327a69d05e20b43869caba76a2f7323f6
Michael <m.vescovo@gmail.com>
2016-11-27 19:23:08 +1100

    feat: Add a few more jokes so there isn't just one

--- jokes/src/main/java/com/example/Joker.java
+++ jokes/src/main/java/com/example/Joker.java
@@ -1,8 +1,25 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Joker {
 
+    private static List<String> jokes;
+
+    static {
+        jokes = new ArrayList<>();
+        jokes.add("The joke engine is experiencing jovial difficulties.");
+        jokes.add("iProducts are cool!");
+        jokes.add("Global warming is a hoax.");
+        jokes.add("Any of the following are real: Father Christmas, the Easter bunny, God.");
+        jokes.add("Idiotbook is not a virus.");
+    }
+
     public String getJoke() {
-        return "The joke engine is experiencing jovial difficulties.";
+        Random rand = new Random();
+        int  n = rand.nextInt(5);
+        return jokes.get(n);
     }
 }
